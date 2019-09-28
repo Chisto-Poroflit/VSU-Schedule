ee80b78f0895f2a2b187d8035e2bb25869a0162a
Дмитрий Донских <steamdiver@yandex.ru>
2019-09-28 14:35:19 +0300

    add new files

---
+++ VSU-Schedule/VSU-Schedule/Areas/Identity/Data/VSU_ScheduleUserContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using VSU_Schedule.Areas.Identity.Data;
+
+namespace VSU_Schedule
+{
+    public class VSU_ScheduleUserContext : IdentityDbContext<VSU_ScheduleUser>
+    {
+        public VSU_ScheduleUserContext(DbContextOptions<VSU_ScheduleUserContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ VSU-Schedule/VSU-Schedule/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using VSU_Schedule.Areas.Identity.Data;
+using VSU_Schedule;
+
+[assembly: HostingStartup(typeof(VSU_Schedule.Areas.Identity.IdentityHostingStartup))]
+namespace VSU_Schedule.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<VSU_ScheduleUserContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("VSU_ScheduleUserContextConnection")));
+
+                services.AddDefaultIdentity<VSU_ScheduleUser>()
+                    .AddDefaultUI(UIFramework.Bootstrap4)
+                    .AddEntityFrameworkStores<VSU_ScheduleUserContext>();
+            });
+        }
+    }
+}
