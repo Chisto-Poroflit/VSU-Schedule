9c283c57bfffb6732d13fb6294063012584155a8
SteamDiver <steamdiver@yandex.ru>
2019-10-16 19:00:51 +0300

    groups table timetable

---
+++ VSU-Schedule/VSU-Schedule/Areas/Timetable/Pages/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DbLibrary;
+using DbLibrary.Models.Entity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace VSU_Schedule.Areas.Timetable.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private ApplicationContext _context;
+
+        public IndexModel(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public List<Para> Para { get; private set; }
+        public List<Group> Groups { get; private set; }
+
+        public void OnGet()
+        {
+            Para = _context.Para.ToList();
+            Groups = _context.Groups.ToList();
+        }
+    }
+}
