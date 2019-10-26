2fdc8eecc9dbf204c0e92cd9301c7075e4c36964
Дмитрий Донских <steamdiver@yandex.ru>
2019-10-26 08:32:05 +0300

    add new files

---
+++ VSU-Schedule/DbLibrary/Models/Entity/TeacherSubject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace DbLibrary.Models.Entity
+{
+    public class TeacherSubject
+    {
+        public int TeacherId { get; set; }
+
+        public Teacher Teacher { get; set; }
+
+        public int SubjectId { get; set; }
+        [Display(Name = "Предметы")]
+        public Subject Subject { get; set; }
+    }
+}
